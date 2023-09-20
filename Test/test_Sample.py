27312f1a49d74083700678b81eeb08412559faf1
dinesh-08-12 <dinesh.j.kmu.8@gmail.com>
2023-09-20 17:49:55 +0530

    WorkCompleted

---
+++ Test/test_Sample.py
@@ -0,0 +1,21 @@
+
+
+
+
+class Test_SocialMedias():
+
+    def test_facbook(self):
+        assert False
+        print("---FACEBOOK---")
+
+    def test_whatsapp(self):
+        print("---WHATSAPP---")
+
+    def test_instgram(self):
+        print("---INSTAGRAM---")
+
+    def test_twitter(self):
+        print("---TWITTER---")
+
+    def test_snapchat(self):
+        print("---SNAPCHAT---")
